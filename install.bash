a7c2f7fa185a93d5507177a57de715b63c982a25
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2016-10-27 10:59:21 +0100

    Update installer script to work on Linux and OSX

---
+++ install.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  config_dirs_template="$HOME/Library/Preferences/XXX*"
+else
+  config_dirs_template="$HOME/.XXX*/config"
+fi
+
+shopt -s nullglob # to prevent the config_dir glob misbehaving below
+
+for product in CLion DataGrip IntelliJ IdeaIC RubyMine PhpStorm WebStorm AndroidStudio PyCharm; do
+  config_dirs="${config_dirs_template//XXX/$product}"
+  for config_dir in $config_dirs; do
+    colors_dir="$config_dir/colors"
+    echo "Installing in $colors_dir"
+    mkdir -p "$colors_dir"
+    cp Gruvbox.icls "$colors_dir"
+  done
+done
